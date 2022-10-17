32be5f7492476d806eb47f86231377c930a107fe
sotaart <sotaart@yandex.ru>
2022-10-18 00:07:42 +0300

    Дз 9 Калмыков Сергей Сергеевич

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// Софья, спасибо за работу! Было познавательно!
+
+/*
+// Задача 64: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+//     N = 5 -> "5, 4, 3, 2, 1"
+//     N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Задача 64:");
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(FromNto1(N));
+string FromNto1(int n)
+{
+    if (n == 1) return "1";
+    return n + " " + FromNto1(n - 1);
+}
+Console.WriteLine();
+*/
+
+
+
+
+
+/*
+// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+//     M = 1; N = 15 -> 120
+//     M = 4; N = 8. -> 30
+Console.WriteLine("Задача 66:");
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumFromMtoN(m, n));
+int SumFromMtoN(int m, int n)
+{
+    if (n == m) return m;
+    return n + SumFromMtoN(m, n-1);
+}
+Console.WriteLine();
+*/
+
+
+
+
+
+
+/*
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+//     m = 2, n = 3 -> A(m,n) = 9
+//     m = 3, n = 2 -> A(m,n) = 29
+Console.WriteLine("Задача 68:");
+Console.Write("Введите число mA: ");
+int mA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число nA: ");
+int nA = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Ackermann(mA, nA));
+int Ackermann(int m, int n)
+{
+    if(m == 0 && n > 0) return n + 1;
+    if(m > 0 && n == 0) return Ackermann(m - 1, 1);
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+*/
